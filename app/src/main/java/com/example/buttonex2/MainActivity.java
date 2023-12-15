06e9b0e406f2b6f6a547a80305158e8814ae5b39
ay4748 <ay4748@bs.amalnet.k12.il>
2023-12-15 21:15:17 +0200

    final!

--- app/src/main/java/com/example/buttonex2/MainActivity.java
+++ app/src/main/java/com/example/buttonex2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.buttonex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void dodge(View view) {
+        btn=findViewById(R.id.button);
+        counter+=1;
+        btn.setText("This is a click number:"+counter);
+
     }
 }
